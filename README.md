bfaac57143f902796c2e7f94f7ca52e8f40ce3a7
Marc Knaup <marc@knaup.io>
2020-07-02 15:42:10 +0200

    🎉 0.9.1

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Installation
 `build.gradle.kts`:
 ```kotlin
 dependencies {
-    implementation("io.fluidsonic.pdf:fluid-pdf:0.9.0")
+    implementation("io.fluidsonic.pdf:fluid-pdf:0.9.1")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.0.12"
 }
 
-fluidJvmLibrary(name = "pdf", version = "0.9.0")
+fluidJvmLibrary(name = "pdf", version = "0.9.1")
 
 fluidJvmLibraryVariant(JvmTarget.jdk8) {
 	description = "Easy PDF generation with HTML & CSS using Chromium or Google Chrome"
