7233efde510a77befaf938c650150a04cffddbbd
Marc Knaup <marc@knaup.io>
2021-02-05 01:20:18 +0100

    🎉 0.14.1-kotlin-1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.18"
 }
 
-fluidLibrary(name = "pdf", version = "0.14.0")
+fluidLibrary(name = "pdf", version = "0.14.1-kotlin-1.5")
 
 fluidLibraryModule(description = "Easy PDF generation with HTML & CSS using Chromium or Google Chrome") {
 	targets {
