35091aa2c4938680c5c6bce17c3315ece2e8b0ed
Marc Knaup <marc@knaup.io>
2025-10-21 06:25:50 +0700

    🎉 0.24.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "2.0.2"
 }
 
-fluidLibrary(name = "pdf", version = "0.23.0")
+fluidLibrary(name = "pdf", version = "0.24.0")
 
 fluidLibraryModule(description = "Easy PDF generation with HTML & CSS using Chromium or Google Chrome") {
 	noDokka()
